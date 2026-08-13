e37214d868425bcaf4a7996ea17229d8105d9f99
Aayush Balami <balamiayush913@gmail.com>
2026-08-13 16:51:55 +0545

    Initial Playwright setup

--- tests/ArrayDivenTesting.spec.js
+++ tests/ArrayDivenTesting.spec.js
@@ -1,16 +1,16 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
-const user = [
+const users=[
     {username: 'standard_user', expectSuccess: true},
     {username: 'locked_out_user', expectSuccess: false},
     {username: 'problem_user', expectSuccess: true},
     {username: 'performance_glitch_user', expectSuccess: true},
     {username: 'error_user', expectSuccess: true},
-    {username: 'virtual_user', expectSuccess: true},
+    {username: 'visual_user', expectSuccess: true},
 ];
 
 for (const user of users) {
-    test(`login as ${user.username}`, async ({ page }) => {
+    test(`login test for ${user.username}`, async ({ page }) => {
         await page.goto('https://www.saucedemo.com/');
         await page.fill('#user-name', user.username);
         await page.fill('#password', 'secret_sauce');
@@ -18,7 +18,7 @@ for (const user of users) {
 
         if (user.expectSuccess) {
             await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-        } else {
+        } else {    
             await expect(page.getByText('Epic sadface: Sorry, this user has been locked out.')).toBeVisible();
         }
     });
